2c401b96ad82e3036f352c0dce51a0334eab933d
Adam Byrd <ajbyrd0723@gmail.com>
2019-12-09 22:28:10 -0600

    file structure and login

---
+++ src/components/auth/Login.js
@@ -0,0 +1,82 @@
+
+
+import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager'
+
+class Login extends Component {
+
+    state = {
+        email: "",
+        password: ""
+    }
+
+
+    handleFieldChange = (evt) => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    componentDidMount() {
+        if (this.props.isAuthenticated()) {
+            this.props.history.push("/home")
+        }
+    }
+
+    handleLogin = (evt) => {
+        evt.preventDefault()
+        APIManager.getAll(`users?email=${this.state.email}`)
+            .then((userInfo) => {
+                if (userInfo.length !== 0) {
+                    if (this.state.password === userInfo[0].password) {
+                        const authObj = {
+                            email: this.state.email,
+                            password: this.state.password,
+                            userId: userInfo[0].id,
+                            name: userInfo[0].username
+                        }
+                        this.props.setUser(authObj)
+                        this.props.history.push("/home")
+                    } else {
+                        window.alert("Please enter the correct password")
+                    }
+                } else {
+                    window.alert("Please enter the correct email address")
+                }
+            })
+    }
+
+    render() {
+        return (
+            <form  onSubmit={this.handleLogin}>
+            <h3>Please enter login info</h3>
+              <fieldset>
+                <label htmlFor="inputEmail" >Email Address</label>
+                <input
+                  onChange={this.handleFieldChange} 
+                  type="email"
+                  id="email"
+                  placeholder="Email address"
+                  required="" autoFocus="" />
+              </fieldset>
+              <fieldset>
+                <label htmlFor="inputPassword">Password</label>
+                <input
+                  onChange={this.handleFieldChange} 
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  required="" />
+              </fieldset>
+            <button 
+              type="submit">
+                Log in
+            </button>
+          </form>
+        )
+    }
+
+
+}
+
+export default Login
---
+++ src/components/auth/RegistrationForm.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager'
+
+class RegistrationForm extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        loadingStatus: false,
+    }
+
+    handleFieldChange = (evt) => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    handleRegistration = (entry) => {
+        entry.preventDefault()
+        if (this.state.username === "" || this.state.email === "" || this.state.password === "") {
+            window.alert("Please complete all fields")
+        } else {
+            this.setState({ loadingStatus: true })
+            const registrationInfo = {
+                username: this.state.username,
+                email: this.state.email,
+                password: this.state.password,
+            }
+            APIManager.post("users", registration)
+                .then(() =>
+                    APIManager.getAll(`users?email=${this.state.email}`)
+                        .then((newUser) => {
+                            this.props.setUser({
+                                email: this.state.email,
+                                password: this.state.password,
+                                userId: newUser[0].id,
+                                name: newUser[0].username
+                            })
+                            this.props.history.push("/messages")
+                        })
+
+                )
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleRegistration}>
+                <h3>Register an Account</h3>
+                <fieldset>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        onChange={this.handleFieldChange}
+                        type="username"
+                        id="username"
+                        placeholder="Full Name"
+                        required="" autoFocus="" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        onChange={this.handleFieldChange}
+                        type="email"
+                        id="email"
+                        placeholder="Email Address"
+                        required="" autoFocus="" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        onChange={this.handleFieldChange}
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        required="" autoFocus="" />
+                </fieldset>
+                <button
+                    type="submit">
+                    Register
+                </button>
+            </form>
+        )
+    }
+}
+
+export default RegistrationForm 
