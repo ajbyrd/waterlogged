983a899292fa336939842b4ebcdb15bb6bdcc678
Adam Byrd <ajbyrd0723@gmail.com>
2019-12-11 09:27:39 -0600

    tracking APIManager

---
+++ src/components/module/APIManager.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+const remoteURL = "http://localhost:5002"
+
+class APIManager extends Component {
+
+
+    get(page, id) {
+        return fetch(`${remoteURL}/${page}/${id}`)
+            .then(r => r.json())
+    }
+
+    getAll(page) {
+        return fetch(`${remoteURL}/${page}`)
+            .then(r => r.json())
+    }
+
+    post(page, newItem) {
+        return fetch(`${remoteURL}/${page}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newItem)
+        }).then(data => data.json())
+    }
+
+    put(page, id, editedItem) {
+        return fetch(`${remoteURL}/${page}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(editedItem)
+        }).then(data => data.json())
+    }
+
+    delete(page, id) {
+        return fetch(`${remoteURL}/${page}/${id}`, {
+            method: "DELETE",
+        }).then(data => data.json())
+    }
+
+}
+
+export default APIManager
