c16b1432083b19233ec106e9adbecc2aae0ce2c4
Adam Byrd <ajbyrd0723@gmail.com>
2020-01-04 10:33:55 -0600

    added image functions

--- src/components/trips/TripDetail.js
+++ src/components/trips/TripDetail.js
@@ -12,7 +12,8 @@ class TripDetail extends Component {
         fish: '',
         season: '',
         locationId: '',
-        loadingStatus: true
+        loadingStatus: true,
+        imageUrl: ''
     }
 
     componentDidMount() {
@@ -26,7 +27,9 @@ class TripDetail extends Component {
                     fish: trip.location.fish,
                     season: trip.season.season,
                     locationId: Number(trip.locationId),
-                    loadingStatus: false
+                    loadingStatus: false,
+                    imageUrl: trip.imageUrl
+
                 });
             });
     }
@@ -34,22 +37,34 @@ class TripDetail extends Component {
     render() {
         return (
             <>
-                <div>
-                    <h1>{this.state.waterName}</h1>
-                    <h3>{this.state.date}</h3>
-                    {/* <img>{this.state.imageUrl}</img> */}
+                <div className="add-button">
+                    <button
+                        className="b ph3 pv2 input-reset ba ml3 b--black bg-transparent grow pointer f6 dib"
+                        type="button" onClick={() => { this.props.history.push(`/trips/${this.props.tripId}/edit`) }}>Edit Trip Details</button>
                 </div>
-                <aside>
-                    <section>Water Access: {this.state.waterAccess}</section>
-                    <section>Gear: {this.state.gear}</section>
-                    <section>Fish: {this.state.fish}</section>
-                    <section>Season: {this.state.season}</section>
-                    <button 
-                    className="b ph3 pv2 input-reset ba ml3 b--black bg-transparent grow pointer f6 dib"
-                    type="button" onClick={() => {this.props.history.push(`/trips/${this.props.tripId}/edit`)}}>Edit Trip Details</button>
-                </aside>
+                <div id="outer-div" className="cf pa3 pa4-m pa5-l mw9 center">
+                    <div id="info-div">
+                        <h1 className="f2 f1-l lh-title mt0 mb4 mb1-ns" >{this.state.waterName}</h1>
+                        <h2 className="dn db-ns">{this.state.date}</h2>
+
+                        <div>Water Access: {this.state.waterAccess}</div>
 
 
+                        <div>Gear: {this.state.gear}</div>
+
+
+                        <div>Fish: {this.state.fish}</div>
+
+
+                        <div>Season: {this.state.season}</div>
+
+                    </div>
+                    <div id="img-div" >
+                        {(this.state.imageUrl !== "")
+                            ? <><img className="db measure center" src={this.state.imageUrl} alt={this.state.waterName} /><br /></>
+                            : null}
+                    </div>
+                </div>
             </>
         )
     }
