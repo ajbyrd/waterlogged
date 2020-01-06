4e11043d8967ca0227a56f62de22f604293b43fb
Adam Byrd <ajbyrd0723@gmail.com>
2020-01-05 21:43:07 -0600

    more styling

--- src/components/trips/TripDetail.js
+++ src/components/trips/TripDetail.js
@@ -42,8 +42,8 @@ class TripDetail extends Component {
                         className="b ph3 pv2 input-reset ba ml3 b--black bg-transparent grow pointer f6 dib"
                         type="button" onClick={() => { this.props.history.push(`/trips/${this.props.tripId}/edit`) }}>Edit Trip Details</button>
                 </div>
-                <div id="outer-div" className="cf pa1 pa1-m pa1-l mw8 mb4 center ba b--light-silver br3">
-                    <div id="info-div">
+                <div id="outer-div" className="cf pa1 pa1-m pa1-l mw8 mb4 center ba bw2 b--dark-green br3">
+                    <div id="info-div" className="">
                         <h1 className="f2 f1-l lh-title mt0 mb4 mb1-ns" >{this.state.waterName}</h1>
                         <h2 className="dn db-ns">{this.state.date}</h2>
 
@@ -59,9 +59,9 @@ class TripDetail extends Component {
                         <div>Season: {this.state.season}</div>
 
                     </div>
-                    <div id="img-div" >
+                    <div id="img-div" className="center" >
                         {(this.state.imageUrl !== "")
-                            ? <><img className="db measure center" src={this.state.imageUrl} alt={this.state.waterName} /><br /></>
+                            ? <><img className="db mt4 mr2 measure center" src={this.state.imageUrl} alt={this.state.waterName} /><br /></>
                             : null}
                     </div>
                 </div>
